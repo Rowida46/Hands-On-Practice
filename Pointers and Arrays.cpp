2c63a8bbbe4b71e7ff73e057c86fff61c6ae7952
Rowida46 <52299389+Rowida46@users.noreply.github.com>
2019-08-11 03:43:31 +0200

    Update Pointers and Arrays.cpp

--- Pointers and Arrays.cpp
+++ Pointers and Arrays.cpp
@@ -11,6 +11,12 @@ int main(){
  for ( int y=0 ; y <=3 ; y++)
  cout<<*(a+y) <<endl;
  //cout<<a ; the oupt will be the address of a 
+	
+	
+   int c[3] = {5,6,7};
+	int *b; b = c;
+	for ( int x=0 ; x<3 ; x++)
+	cout<<*(b++)<<endl;
 return 0;	
 
 }
