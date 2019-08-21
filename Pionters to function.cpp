2098c4146f38c6dd3e8e77f2646c7c3ab8873f8a
Rowida46 <52299389+Rowida46@users.noreply.github.com>
2019-08-21 17:33:12 +0200

    Add files via upload

---
+++ Pionters to function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ void ntoone (int a){
+ 	for (int i =1 ; i <=a ; i++)
+ 	cout<<i<<" ";
+ }
+ void compute(int A , (*fun) (int ))
+	cout<<  (*fuc )(A ) ;
+ 	
+int main()
+{
+	int n ;
+	cin>>n;
+		  
+		  compute(n , ntoone);
+	
+	
+}
